6cb89cf1b209603a48f898038ab41dec830f77dc
Deeksha Prabhakar <deekiyengar@gmail.com>
2016-07-03 02:35:28 -0700

    Lecture 1

--- CS193p/Calculator/Calculator/ViewController.swift
+++ CS193p/Calculator/Calculator/ViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!//initially set to nil, implicitly unwrapped optional
+    
+    var userIsInMiddleOfTyping = false //variables should always be initialized
+    
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        
+        if(userIsInMiddleOfTyping){
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else{
+            display.text = digit
+        }
+        userIsInMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle { //mathematicalSymbol only exists in the current scope
+            if(mathematicalSymbol == "π"){
+                display.text = String(M_PI)
+            }
+        }
     }
-
-
 }
 
